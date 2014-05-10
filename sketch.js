1f543ff149ca19077f3fc97b1debd72135d82742
RDard <robdardenelle@gmail.com>
2014-05-10 16:28:38 -0700

    Custom grid size added, redraw needs work

--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
+if (typeof gridSize === 'undefined'){
+	var gridSize = 16;
+}
+var maxPixels = 960;
+
 $(document).ready(function(){
-	for (var i = 0; i < 16; i++) {
+
+	for (var i = 0; i < gridSize; i++) {
 		$("#container").append("<div class='row'></div>");
 	};	
 		$(".row").each(function(){
-			for (var j = 0; j < 16; j++) {
+			for (var j = 0; j < gridSize; j++) {
 				$(this).append("<div class='square'></div>");
 				};
 		});
@@ -11,8 +17,19 @@ $(document).ready(function(){
 	$('.square').mouseenter(function(){
 		$(this).addClass("draw");
 	});
-});
 
+	$('button').on('click', function (){
+		$('.square').removeClass("draw");
+		var gridSize = prompt("Enter the number of squares per side for the drawing grid");
+		if (gridSize > 960 || gridSize < 1)
+			gridSize = 16;
+
+		$('.row').css({'height':''+ maxPixels/gridSize +'px'});
+		$('.square').css({'height':''+ maxPixels/gridSize +'px', 
+										'width':''+ maxPixels/gridSize +'px'});
+	
+	});
+});
 
 
 
