2ee58b00fd365af543308716922b650dc421b8e5
RDard <robdardenelle@gmail.com>
2014-05-10 17:51:39 -0700

    Redraw fixed

--- sketch.js
+++ sketch.js
@@ -1,36 +1,44 @@
-if (typeof gridSize === 'undefined'){
-	var gridSize = 16;
-}
 var maxPixels = 960;
 
 $(document).ready(function(){
+	if (typeof gridSize === 'undefined'){
+		var gridSize = 16;
+		console.log(gridSize);
+	}
 
-	for (var i = 0; i < gridSize; i++) {
-		$("#container").append("<div class='row'></div>");
-	};	
-		$(".row").each(function(){
-			for (var j = 0; j < gridSize; j++) {
-				$(this).append("<div class='square'></div>");
-				};
+	function gridDraw(gridSize){
+		console.log(gridSize);
+		for (var i = 0; i < gridSize; i++) {
+			$("#container").append("<div class='row'></div>");
+		};	
+			$(".row").each(function(){
+				for (var j = 0; j < gridSize; j++) {
+					$(this).append("<div class='square'></div>");
+					};
+			});
+		
+		$('.square').mouseenter(function(){
+			$(this).addClass("draw");
 		});
+	}
+	gridDraw(gridSize);
 
-	$('.square').mouseenter(function(){
-		$(this).addClass("draw");
-	});
+	$('button').click(function (){
+		$('.square').remove();
+		$('.row').remove();
 
-	$('button').on('click', function (){
-		$('.square').removeClass("draw");
 		var gridSize = prompt("Enter the number of squares per side for the drawing grid");
 		if (gridSize > 960 || gridSize < 1)
 			gridSize = 16;
 
+		gridDraw(gridSize);
+
 		$('.row').css({'height':''+ maxPixels/gridSize +'px'});
 		$('.square').css({'height':''+ maxPixels/gridSize +'px', 
 										'width':''+ maxPixels/gridSize +'px'});
-	
+		
 	});
+
+	
 });
 
-
-
-alert("test2");
